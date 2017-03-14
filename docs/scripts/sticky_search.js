52e1dc421f2cba0f643863f252622bffb57062fd
danilyanich <danilyanich@gmail.com>
2017-03-14 19:55:30 +0300

    Removed kostyl in pixels. Search now sticks relative to its wrap.

--- docs/scripts/sticky_search.js
+++ docs/scripts/sticky_search.js
@@ -1,14 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', function(event) {
     var searchBar = document.querySelector('div.search-bar.v-align.animatable');
-    //var content = document.getElementById('post-placeholder');
+    var pass = document.querySelector('html>body>div.middle-wrap>div.middle-content>div.search-bar-wrap');
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 430) {
+        if (pass.getBoundingClientRect().top < 0) {
             searchBar.classList.add('search-bar-fixed');
-            //content.classList.add('blur');
         } else {
             searchBar.classList.remove('search-bar-fixed');
-            //content.classList.remove('blur');
         }
     });
 });
