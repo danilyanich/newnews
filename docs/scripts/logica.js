3d7670d8b320a47cb61cbc6f109ef1a636f3d61f
danilyanich <danilyanich@gmail.com>
2017-03-15 15:22:45 +0300

    add/edit form resets on open. warns u if content is unsaved.

--- docs/scripts/logica.js
+++ docs/scripts/logica.js
@@ -5,29 +5,47 @@ var logica = (function () {
 
     document.addEventListener('DOMContentLoaded', function (event) {
 
-        document.getElementById('plus').addEventListener('click', function (event) {
+        // plus button opens add form, and sets it's content to default
+        var openForm = function (event) {
             document.querySelector('body').style.overflowY = 'hidden';
             document.querySelector('div.fullscreen-scrollable-wrap').style.display = 'block';
             document.forms.edit.querySelector('.info-bar').innerHTML = (new Date()).prettyFormat() + ' by' +
             '<span>' + authorization.getUser() + '</span>';
-        });
+            document.forms.edit.caption.value = '';
+            document.forms.edit.summary.value = '';
+            document.forms.edit.content.value = '';
+            document.forms.edit.tags.value = '';
+        }
+        document.getElementById('plus').addEventListener('click', openForm);
 
-        var resize = function (event){
+        // textareas are resizing to fit content
+        var resize = function (event) {
             event.currentTarget.style.height = event.currentTarget.scrollHeight +'px';
         }
         for (var textarea of document.querySelectorAll('textarea')) {
             textarea.addEventListener('keypress', resize);
         }
 
-        document.querySelector('div.fullscreen-scrollable-wrap').addEventListener('click', function (event) {
+        // click on wrap / cross button closes the form
+
+        var checkUnsaved = function() {
+            return !(document.forms.edit.caption.value === '' &&
+                document.forms.edit.summary.value === '' &&
+                document.forms.edit.content.value === '' &&
+                document.forms.edit.tags.value === '');
+        }
+
+        var closeForm = function (event) {
+
             var fswrap = document.querySelector('div.fullscreen-scrollable-wrap');
             var mwrap = document.querySelector('div.fullscreen-scrollable-wrap div.middle-wrap');
             var cross = document.querySelector('div.fullscreen-scrollable-wrap div.middle-wrap svg.light-svg');
             if(event.target === fswrap || event.target === mwrap || event.target === cross) {
+                if (checkUnsaved()) alert('unsaved form');
                 document.querySelector('body').style.overflowY = 'scroll';
                 fswrap.style.display = 'none';
             }
-        });
-
+        }
+        document.querySelector('div.fullscreen-scrollable-wrap').addEventListener('click', closeForm);
     });
 })();
