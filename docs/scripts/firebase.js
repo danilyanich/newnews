c073dde14f25b947be2370cde8bd0f3bf0a0b2ec
danilyanich <danilyanich@gmail.com>
2017-03-16 23:15:26 +0300

    posts are saved to firebase

---
+++ docs/scripts/firebase.js
@@ -0,0 +1,25 @@
+var database = (function () {
+    // initialize database
+    document.addEventListener('DOMContentLoaded', function (event) {
+        firebase.database().ref('/posts/').once('value').then(function (snapshot) {
+            var articles = snapshot.val();
+            for(var index in articles) {
+                articles[index].createdAt = new Date(parseInt(articles[index].createdAt));
+                articles[index].id = index;
+                data.add(articles[index]);
+            }
+            dom.display(data.getMultiple(null, {all: true}));
+        })
+    })
+
+    var addArticle = function (post) {
+        var copy = deepCopy(post);
+        delete copy.id;
+        copy.createdAt = copy.createdAt.getTime();
+        firebase.database().ref('/posts/' + post.id).set(copy);
+    }
+
+    return {
+        addArticle: addArticle
+    }
+})();
