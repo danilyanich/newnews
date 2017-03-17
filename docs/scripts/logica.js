36db1cc59f4e0eb2f676fea007a6f5dea220a021
danilyanich <danilyanich@gmail.com>
2017-03-18 01:32:12 +0300

    code reorganzed with closures. auth form logica added.

--- docs/scripts/logica.js
+++ docs/scripts/logica.js
@@ -3,9 +3,31 @@ var logica = (function () {
 
     HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 
-    document.addEventListener('DOMContentLoaded', function (event) {
+    var openFullscreenWrap = function () {
+        document.querySelector('body').style.overflowY = 'hidden';
+        var fswrap = document.querySelector('div.fullscreen-scrollable-wrap');
+        fswrap.style.display = 'flex';
+        return fswrap;
+    }
 
+    var closeFullscreenWrap = function(fswrap) {
+        document.querySelector('body').style.overflowY = 'scroll';
+        fswrap.style.display = 'none';
+        fswrap.innerHTML = '';
+    }
 
+    var closeFullscreenWrapEvent = function (event) {
+        var fswrap = document.querySelector('div.fullscreen-scrollable-wrap');
+        var mwrap = document.querySelector('div.fullscreen-scrollable-wrap div.middle-wrap');
+
+        if(event.target === fswrap || event.target === mwrap) {
+            closeFullscreenWrap(fswrap);
+        }
+    }
+
+    var addEditForm = (function () {
+
+        var editAddForm = document.getElementById('edit-add-form-template');
 
         // saving article
         var saveArtice = function (event) {
@@ -21,9 +43,9 @@ var logica = (function () {
                 content: document.forms.edit.content.value,
                 tags: tagsline.split(' ')
             }
+            var result = dom.add(article);
             var image = document.querySelector('#dragndrop img');
             if (image) article.image = image.src;
-            var result = dom.add(article);
             if (result) {
                 database.addArticle(article);
                 document.querySelector('span.form-button.discard').click();
@@ -31,8 +53,6 @@ var logica = (function () {
                 alert('invalid article');
             }
         }
-
-
 
         // dragndrop emplementation
         function allowDrop(event) {
@@ -42,20 +62,60 @@ var logica = (function () {
         function drop(event) {
             event.preventDefault();
             var path = event.dataTransfer.getData('text');
-            var img = document.createElement('img');
-            img.src = path;
-            event.currentTarget.innerHTML = '';
-            event.currentTarget.appendChild(img);
+            var image = document.createElement('img');
+            var callback = function (exists) {
+                if(exists) {
+                    var container = document.querySelector('#dragndrop');
+                    container.innerHTML = '';
+                    container.appendChild(image);
+                } else {
+                    alert('only links to images allowed');
+                }
+            }
+            image.onerror = function () { callback(false); };
+            image.onload = function () { callback(true); };
+            image.src = path;
         }
-
-
 
         // textareas are resizing to fit content
         var resize = function (event) {
             event.currentTarget.style.height = event.currentTarget.scrollHeight +'px';
         }
 
+        // click on wrap / cross button closes the form
+        var checkUnsaved = function() {
+            return !(document.forms.edit.caption.value === '' &&
+                document.forms.edit.summary.value === '' &&
+                document.forms.edit.content.value === '' &&
+                document.forms.edit.tags.value === '');
+        }
 
+        var closeAddEditForm = function (event) {
+            var cross = document.querySelector('div.fullscreen-scrollable-wrap div.middle-wrap svg.light-svg');
+            var discard = document.querySelector('span.form-button.discard');
+            if(event.target === cross || event.target === discard || event.target.contains(cross)) {
+                if (checkUnsaved() && event.target !== discard) alert('unsaved form');
+                var fswrap = document.querySelector('div.fullscreen-scrollable-wrap');
+                fswrap.removeEventListener('click', closeAddEditForm);
+                closeFullscreenWrap(fswrap);
+            }
+        }
+
+        // plus button opens add form, and sets it's content to default
+        var openAddEditForm = function (event) {
+            var copy = editAddForm.content.cloneNode(true);
+            var form = copy.querySelector('form');
+
+            form.querySelector('.info-bar').innerHTML =
+                (new Date()).prettyFormat() + ' by' +
+                '<span>' + authorization.getUser() + '</span>';
+
+            var fswrap = openFullscreenWrap();
+
+            fswrap.addEventListener('click', closeAddEditForm);
+            linkAddEditFormEvents(copy);
+            fswrap.appendChild(copy);
+        }
 
         // link all events
         function linkAddEditFormEvents(form) {
@@ -67,52 +127,70 @@ var logica = (function () {
             }
         }
 
-
-
-        // click on wrap / cross button closes the form
-        var checkUnsaved = function() {
-            return !(document.forms.edit.caption.value === '' &&
-                document.forms.edit.summary.value === '' &&
-                document.forms.edit.content.value === '' &&
-                document.forms.edit.tags.value === '');
+        return {
+            open: openAddEditForm
         }
 
-        var closeForm = function (event) {
-            var fswrap = document.querySelector('div.fullscreen-scrollable-wrap');
-            var mwrap = document.querySelector('div.fullscreen-scrollable-wrap div.middle-wrap');
-            var cross = document.querySelector('div.fullscreen-scrollable-wrap div.middle-wrap svg.light-svg');
-            var discard = document.querySelector('span.form-button.discard');
-            if(event.target === fswrap || event.target === mwrap || event.target === cross || event.target === discard) {
-                if (checkUnsaved() && event.target !== discard) alert('unsaved form');
-                document.querySelector('body').style.overflowY = 'scroll';
-                fswrap.style.display = 'none';
-                fswrap.innerHTML = '';
+    })();
 
-                document.querySelector('div.fullscreen-scrollable-wrap').removeEventListener('click', closeForm);
+    var authorizationForm = (function () {
+
+        var authorizationForm = document.getElementById('authorization-form-template');
+
+        var closeAuthorizationForm = function (event) {
+            var form =  event.currentTarget.querySelector('form');
+            var button = event.currentTarget.querySelector('form > button');
+
+            if (event.target == button) {
+                var login = form.login.value;
+                var password = form.password.value;
+                firebase.auth().signInWithEmailAndPassword(login, password).then(function () {
+                    authorization.changeUser(login.replace(/@.*/g,''));
+                    authorization.authorize();
+                    var fswrap = document.querySelector('div.fullscreen-scrollable-wrap');
+                    fswrap.removeEventListener('click', closeAuthorizationForm);
+                    closeFullscreenWrap(fswrap);
+                }).catch(function(error) {
+                    alert(error.code + ':' + error.message);
+                });
             }
         }
 
-
-
-        // plus button opens add form, and sets it's content to default
-        var editAddForm = document.getElementById('edit-add-form-template');
-
-        var openForm = function (event) {
-            var copy = editAddForm.content.cloneNode(true);
-            var form = copy.querySelector('form');
-
-            form.querySelector('.info-bar').innerHTML =
-                (new Date()).prettyFormat() + ' by' +
-                '<span>' + authorization.getUser() + '</span>';
-
-            document.querySelector('body').style.overflowY = 'hidden';
-            var fswrap = document.querySelector('div.fullscreen-scrollable-wrap');
-            fswrap.style.display = 'flex';
-            fswrap.addEventListener('click', closeForm);
-            linkAddEditFormEvents(copy);
-            fswrap.appendChild(copy);
+        var linkEvents = function (form) {
         }
 
-        document.getElementById('plus').addEventListener('click', openForm);
+        var openAuthorizationForm = function (event) {
+            if (authorization.isAuthorized()) {
+                firebase.auth().signOut().then(function() {
+                    alert('logout');
+                    authorization.changeUser(null);
+                    authorization.authorize();
+                }).catch(function(error) {
+                    alert(error.code + ':' + error.message);
+                });
+            } else {
+                var copy = authorizationForm.content.cloneNode(true);
+                var form = copy.querySelector('form');
+
+                var fswrap = openFullscreenWrap();
+
+                fswrap.addEventListener('click', closeAuthorizationForm);
+                linkEvents(copy);
+                fswrap.appendChild(copy);
+            }
+        }
+
+        return {
+            open: openAuthorizationForm
+        }
+
+    })();
+
+    document.addEventListener('DOMContentLoaded', function (event) {
+        // link events
+        document.querySelector('div.fullscreen-scrollable-wrap').addEventListener('click', closeFullscreenWrapEvent);
+        document.getElementById('plus').addEventListener('click', addEditForm.open);
+        document.querySelector('header.header.v-align div.v-align').addEventListener('click', authorizationForm.open);
     });
+
 })();
